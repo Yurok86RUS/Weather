d3e6a62af8bbae1ea9c03a4167235f3df68b4b89
Yurok <yurok.kolesnikov@gmail.com>
2019-11-25 21:40:05 +0500

    Create project

---
+++ app/src/main/java/com/yur0k/weather/WeatherActivity.java
@@ -0,0 +1,14 @@
+package com.yur0k.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+    }
+}
