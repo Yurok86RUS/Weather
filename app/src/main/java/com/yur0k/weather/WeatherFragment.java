3763b22a2e6894d48fcc87ae290521f1ae5eaf0a
Yurok <yurok.kolesnikov@gmail.com>
2019-11-25 21:56:43 +0500

    runned

--- app/src/main/java/com/yur0k/weather/WeatherFragment.java
+++ app/src/main/java/com/yur0k/weather/WeatherFragment.java
@@ -1,6 +1,28 @@
 package com.yur0k.weather;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
 import androidx.fragment.app.Fragment;
 
-class WeatherFragment extends Fragment {
+
+public class WeatherFragment extends Fragment {
+
+    private TextView cityTextView;
+    private TextView nowTemperature;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WeatherActivity activity = (WeatherActivity) getActivity();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_weather, container, false);
+        return rootView;
+    }
 }
